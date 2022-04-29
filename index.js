b1e8cf05ee96759ec0ab3070217fc55444ae4b22
Diana <oganauskiene@gmail.com>
2022-04-29 20:25:37 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+let vard = "Rasa";
+console.log(vard);
+
+let palukanos = 0.5;
+palukanos = 10;
+console.log(palukanos);
+
+let asmuo = {
+  vardas: "Ona",
+  amzius: 25,
+};
+
+asmuo.vardas = "Laura";
+
+let selection = "vardas";
+asmuo[selection] = "Dovile";
+
+console.log(asmuo.vardas);
+
+let spalvos = ["balta", "ruda"];
+spalvos[2] = "zalia";
+console.log(spalvos.length);
